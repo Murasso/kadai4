3dc17cb799b872c17537129dc0bb152850acd86c
Murasso <sou04136@gmail.com>
2023-10-25 13:52:58 +0000

    Finish kadai4

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,11 @@ class Book < ApplicationRecord
    belongs_to :user
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
   def get_profile_image(height, width)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show,:index,:edit,:update]
   
-  resources :books, only: [:edit, :index, :show, :create, :destroy, :update]
- 
+  resources :books, only: [:edit, :index, :show, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+    
+  end
   get "home/about" => "home#about", as: "about"
 end
